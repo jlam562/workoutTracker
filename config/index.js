9a776304d2956cffe1644c2321f8bc6ab66de8e1
jlam <jennylamwork@gmail.com>
2020-04-06 21:34:08 -0700

    fixed mongo

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-module.exports = require('mongoose').connect('',
+module.exports = require('mongoose').connect('mongodb://heroku_6zsj1fkj:7urm6arouju3adehlcsm1urdsp@ds155315.mlab.com:55315/heroku_6zsj1fkj',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
